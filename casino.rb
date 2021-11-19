b497c79679d424484211b19c3f66c96da9b3e61a
Madeleine Whitley <maddy.whitley@gmail.com>
2021-11-18 18:30:47 -0700

    created the file

---
+++ casino.rb
@@ -0,0 +1,9 @@
+# menu: login with name -> balance in wallet 
+# menu with games 
+  # slots
+  # high / low
+  # three cups games 
+# player places bet and wins / loses 
+# player's bankroll goes up and down with wins and losses 
+
+

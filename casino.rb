887713a5ae6e1a891971807e1bc34fe3945d6a1c
Madeleine Whitley <maddy.whitley@gmail.com>
2021-11-18 19:15:02 -0700

    added menu file

--- casino.rb
+++ casino.rb
@@ -6,4 +6,23 @@
 # player places bet and wins / loses 
 # player's bankroll goes up and down with wins and losses 
 
+require_relative 'Person.rb'
+
+class App
+  attr_accessor :Person
+
+  def initialize
+    @person = init_person
+  end
+
+  def init_person
+    puts "Enter your name"
+    name = gets.strip
+    puts "Enter your balance"
+    balance = gets.to_i
+    @person = Person.new(name, balance)
+  end
+end
+
+app = App.new 
 
